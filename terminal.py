e41076bce8a40a5e13eafa0944a2d2cd9ee1c134
Saurabh londhe <saurabhlondhe1111@gmail.com>
2017-09-03 14:19:38 +0545

    Add files via upload
    source code

---
+++ terminal.py
@@ -0,0 +1,10 @@
+import os
+cmnd=1
+import time
+while(cmnd!='exit'):
+	os.system("clear")
+	print('\x1b[6;30;42m' + '\t\tSaurabh Londhe' + '\x1b[0m')
+	os.system("pwd")
+	cmnd=raw_input()
+	os.system(cmnd)
+	time.sleep(1)
